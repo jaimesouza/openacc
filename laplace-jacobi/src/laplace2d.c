0c5297e92c11c0c30c0430e2bfcfea9146d9687e
Jaime Freire de Souza <jaimefreire.souza@gmail.com>
2020-03-05 11:07:03 -0300

    updates

--- laplace-jacobi/src/laplace2d.c
+++ laplace-jacobi/src/laplace2d.c
@@ -13,13 +13,13 @@ int max(float a, float b){
 int main(){
 
     // size of grid (width)
-    const int N = 1000;
+    const int N = 4096;
 
     // size of grid (height)
-    const int M = 1000;
+    const int M = 4096;
 
     // number of maximum iterations
-    const int ITER_MAX = 1000;
+    const int ITER_MAX = 2000;
 
     // threshold of convergence
     const float CONV_THRESHOLD = 1.0e-5f;
@@ -43,6 +43,8 @@ int main(){
     float err = 1;
     int iter = 0;
 
+    printf("Jacobi relaxation Calculation: %d x %d mesh\n", N, M);
+
     struct timeval start_kernel, end_kernel;
     gettimeofday(&start_kernel, NULL);
 
@@ -54,23 +56,23 @@ int main(){
 
         err = 0.0;
 
-        #pragma acc kernels
-        {
-            // calculates the Laplace equation to determine each cell's next value
-            for( int i = 1; i < N-1; i++) {
-                for(int j = 1; j < M-1; j++) {
-                    Anew[i][j] = 0.25 * (A[i][j+1] + A[i][j-1] + A[i - 1][j] + A[i+1][j]);
-                    err = max(err, abs(Anew[i][j] - A[i][j]));
-                }
-            }
-
-            // copies the next values into the working array for the next iteration
-            for( int i = 1; i < N-1; i++) {
-                for( int j = 1; j < M-1; j++ ) {
-                    A[i][j] = Anew[i][j];
-                }
+        // calculates the Laplace equation to determine each cell's next value
+        for( int i = 1; i < N-1; i++) {
+            for(int j = 1; j < M-1; j++) {
+                Anew[i][j] = 0.25 * (A[i][j+1] + A[i][j-1] + A[i - 1][j] + A[i+1][j]);
+                err = max(err, abs(Anew[i][j] - A[i][j]));
             }
         }
+
+        // copies the next values into the working array for the next iteration
+        for( int i = 1; i < N-1; i++) {
+            for( int j = 1; j < M-1; j++ ) {
+                A[i][j] = Anew[i][j];
+            }
+        }
+
+        if(iter % 100 == 0)
+            printf("%5d, %0.6f\n", iter, error);
 
         iter++;
     }
@@ -78,19 +80,7 @@ int main(){
     gettimeofday(&end_kernel, NULL);
     double kernel_time = (double)(end_kernel.tv_sec-start_kernel.tv_sec)+(double)(end_kernel.tv_usec-start_kernel.tv_usec)/1000000;
 
-    printf("Mesh dimension %d x %d: \n", N, M);
-
-    // print out the results
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < M; j++){
-            printf("%.2f ", A[i][j]);
-        }
-        printf("\n");
-    }
-
-    //printf("\nTime to process the kernel: %f seconds\n", kernel_time );
-
-    printf("\nNumber of iterations: %d\n", iter);
+    printf("\nTime to process the kernel: %f seconds\n", kernel_time );
 
     return 0;
 }
